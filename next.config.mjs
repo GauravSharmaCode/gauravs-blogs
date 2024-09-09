e08dd0e9b9500ee3c4f16e4e98a06186ad737203
gaurav_sharma_code <shrma.gurv@gmail.com>
2024-09-09 22:04:06 +0530

    Redraft next.config

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,14 @@ const config = {
     /// Set this to false if you want production builds to abort if there's lint errors
     ignoreDuringBuilds: process.env.VERCEL_ENV === 'production',
   },
+  async rewrites() {
+    return [
+      {
+        source: '/blogs',
+        destination: '/path-to-your-current-blog', // Replace this with your actual blog path
+      },
+    ]
+  },
 }
 
 export default config
